f4df20e0430b5fa3e3113c60f2adbbebfbbaa97b
MatheusElMarge <elmargematheuspaes@gmail.com>
2023-10-23 13:45:27 -0300

    telas adaptadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LayoutComponent } from './components/layout/layout.component';
-import { PedidosComponent } from './components/pedidos/pedidos.component';
-import { ProdutosComponent } from './produtos/produtos.component';
-import { SistemaComponent } from './components/sistema/sistema.component';
 import { LoginComponent } from './components/sistema/login/login.component';
-import { PedidoslistComponent } from './pedidos/pedidoslist/pedidoslist.component';
+import { PedidoslistComponent } from './components/pedidos/pedidoslist/pedidoslist.component';
 import { PedidosdetailsComponent } from './components/pedidos/pedidosdetails/pedidosdetails.component';
 import { ProdutosdetailsComponent } from './components/produtos/produtosdetails/produtosdetails.component';
 import { ProdutoslistComponent } from './components/produtos/produtoslist/produtoslist.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
 import { IndexComponent } from './components/layout/index/index.component';
-
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutComponent,
-    PedidosComponent,
-    ProdutosComponent,
-    SistemaComponent,
+    FormsModule,
     LoginComponent,
     PedidoslistComponent,
     PedidosdetailsComponent,
@@ -31,6 +23,7 @@ import { IndexComponent } from './components/layout/index/index.component';
     HeaderComponent,
     FooterComponent,
     IndexComponent
+    
   ],
   imports: [
     BrowserModule,
--- src/app/components/sistema/login/login.component.ts
+++ src/app/components/sistema/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from 'src/app/models/usuario';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+
+  usuario: Usuario = new Usuario();
+  roteador = inject(Router);
+
+  logar() {
+    if (this.usuario.login == 'admin' && this.usuario.senha == 'admin')
+      this.roteador.navigate(['admin/pedidos']);
+    else
+      alert('Login ou senha incorretos!');
+  }
+
 
 }
