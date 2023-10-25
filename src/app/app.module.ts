b14908a5a22cdb09d336f757871acf3bfc13c9c3
Erick Ojeda <erickojeda2003@hotmail.com>
2023-10-24 21:09:50 -0300

    Estilo e view funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/sistema/login/login.component';
-import { PedidoslistComponent } from './components/pedidos/pedidoslist/pedidoslist.component';
-import { PedidosdetailsComponent } from './components/pedidos/pedidosdetails/pedidosdetails.component';
-import { ProdutosdetailsComponent } from './components/produtos/produtosdetails/produtosdetails.component';
-import { ProdutoslistComponent } from './components/produtos/produtoslist/produtoslist.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
 import { IndexComponent } from './components/layout/index/index.component';
-import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './components/sistema/login/login.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+import { PedidoslistComponent } from './components/pedidos/pedidoslist/pedidoslist.component';
+import { PedidosdetailsComponent } from './components/pedidos/pedidosdetails/pedidosdetails.component';
+import { ProdutoslistComponent } from './components/produtos/produtoslist/produtoslist.component';
+import { ProdutosdetailsComponent } from './components/produtos/produtosdetails/produtosdetails.component';
+
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    IndexComponent,
     LoginComponent,
     PedidoslistComponent,
     PedidosdetailsComponent,
-    ProdutosdetailsComponent,
     ProdutoslistComponent,
-    HeaderComponent,
-    FooterComponent,
-    IndexComponent
-
+    ProdutosdetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule,
-
+    AppRoutingModule,
+    NgbModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
